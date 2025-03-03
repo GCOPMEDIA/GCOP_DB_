5a458d1cf564aa3f78d39c7d7b194657a27c2aa4
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-03 18:28:19 +0000

    deploy

--- GCOP_/admin.py
+++ GCOP_/admin.py
@@ -5,4 +5,19 @@ from .models import *
 
 admin.site.register(AuthUser)
 admin.site.register(AuthGroup)
-# admin
+admin.site.register(AuthGroupPermissions)
+admin.site.register(AuthPermission)
+admin.site.register(AuthUserGroups)
+admin.site.register(AuthUserUserPermissions)
+admin.site.register(Branches)
+admin.site.register(DjangoAdminLog)
+admin.site.register(DjangoContentType)
+admin.site.register(DjangoMigrations)
+admin.site.register(DjangoSession)
+admin.site.register(Groups)
+admin.site.register(JoinedGroups)
+admin.site.register(Member)
+admin.site.register(Positions)
+admin.site.register(Relations)
+admin.site.register(Relationships)
+
