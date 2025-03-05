fb731e96c76d2d80ed3dcf52d94e4cf34e021e17
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-05 17:53:10 +0000

    deploy

--- GCOP_/models.py
+++ GCOP_/models.py
@@ -6,7 +6,8 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.db import models
+from .storages import GoogleDriveStorage
 
 class AuthGroup(models.Model):
     name = models.CharField(unique=True, max_length=150)
@@ -151,10 +152,16 @@ class JoinedGroups(models.Model):
         db_table = 'joinedGroups'
 
 
+google_drive_storage = GoogleDriveStorage()
+
+
+
+
 class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
     f_name = models.CharField(max_length=100)
     l_name = models.CharField(max_length=100)
+    member_image = models.FileField(upload_to="uploads/",null=True,blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     hometown = models.CharField(max_length=100, blank=True, null=True)
     gender = models.CharField(max_length=10, blank=True, null=True)
