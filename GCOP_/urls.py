08089eeb06a41a5bff3f4d40e9637f47f6060970
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-07 14:34:50 +0000

    deploy

--- GCOP_/urls.py
+++ GCOP_/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',login_,name='login_'),
 
-    path('', user_form_view, name='user_form'),
+    path('user_form', user_form_view, name='user_form'),
     path('further_questions/', further_questions_view, name='further_questions'),
     path('child_details/<int:child_index>/', child_details_view, name='child_details'),
     path('father_details/', father_details, name='father_details'),
     path('mother_details/', mother_details, name='mother_details'),
     path('survivor_details/<int:survivor_index>/', survivor_details_view, name='survivor_details'),
     path('form_success/', form_success_view, name='form_success'),
+    path('spouse_details',spouse_details,name='spouse_details')
 ]
