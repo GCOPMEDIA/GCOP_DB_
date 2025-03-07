920f77f803df08fd9959ae18254497ec4003875b
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-07 21:49:38 +0000

    deploy

--- GCOP_/models.py
+++ GCOP_/models.py
@@ -189,7 +189,5 @@ class Relations(models.Model):
     f_name = models.CharField(max_length=100)
     l_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    relationship = models.CharField(max_length=100,blank=True,null=True)
     is_member = models.BooleanField(blank=True, null=True)
-
-class Relationships(models.Model):
-    id()
