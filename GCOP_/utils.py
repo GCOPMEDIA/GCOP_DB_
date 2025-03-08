b9031cdb104da57936145f363cf913247a114b14
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-08 15:37:03 +0000

    deploy

--- GCOP_/utils.py
+++ GCOP_/utils.py
@@ -13,7 +13,8 @@ def member_entry(data):
                gender = data['gender'],
                marital_status=data['marital_status'],
                date_joined = data['date_joined'],
-               welfare_card_num=data['welfare_card_number'],
+
+                welfare_card_num=data['welfare_card_number'],
                tithe_card_num=data['tithe_card_number'],
                history=data['history'],
                baptism_status=data['baptism'],
@@ -23,18 +24,18 @@ def member_entry(data):
         church_branch = Branches.objects.get(branch_name=data['church_branch'])
         m.church_branch = church_branch
         m.save()
-        print('Saved member data')
+        ##print('Saved member data')
 
     for p in (data['position']).split(','):
         position = ChurchPositions(position_name=p,member=m)
         position.save()
-        print('Saved position data')
+        ##print('Saved position data')
 
-    for g in data['groups_joined']:
-        group = Groups.objects.get(group_name=g)
+    for g in data['group_name']:
+        group = Groups.objects.get(group_id=int(g))
         jgroup = Joinedgroups(group=group,member=m)
         jgroup.save()
-        print('Saved group data')
+        ##print('Saved group data')
 
     if data['parent_status']=='Both':
         f = Relations(f_name=data['father_first_name'],
@@ -44,7 +45,7 @@ def member_entry(data):
                       is_member=data['father_is_member'])
         f.member = m
         f.save()
-        print('Saved father data')
+        ##print('Saved father data')
         m = Relations(f_name=data['mother_first_name'],
                       l_name=data['mother_other_name'],
                       phone_number=data['mother_phone_number'],
@@ -52,7 +53,7 @@ def member_entry(data):
                       is_member=data['mother_is_member'])
         m.member = m
         m.save()
-        print('Saved mother data')
+        ##print('Saved mother data')
     elif data['parent_status']=='Only Father':
         f = Relations(f_name=data['father_first_name'],
                       l_name=data['father_other_name'],
@@ -61,7 +62,7 @@ def member_entry(data):
                       is_member=data['father_is_member'])
         f.member = m
         f.save()
-        print('Saved father data')
+        ##print('Saved father data')
     elif data['parent_status']=='Only Mother':
         m = Relations(f_name=data['mother_first_name'],
                       l_name=data['mother_other_name'],
@@ -70,7 +71,7 @@ def member_entry(data):
                       is_member=data['mother_is_member'])
         m.member = m
         m.save()
-        print('Saved mother data')
+        ##print('Saved mother data')
 
 
     for c in range(data['number_of_children']):
@@ -82,10 +83,10 @@ def member_entry(data):
                       is_member=cd['child_is_member'])
         cc.member = m
         cc.save()
-        print('Saved child data')
+        ##print('Saved child data')
 
     for s in range(data['number_of_survivors']):
-        sd = data[f"child_{s+1}"]
+        sd = data[f"survivor_{s+1}"]
         cc = Relations(f_name=sd['survivor_first_name'],
                       l_name=sd['survivor_other_name'],
                       phone_number=sd['survivor_phone_number'],
@@ -93,7 +94,7 @@ def member_entry(data):
                       is_member=sd['survivor_is_member'])
         cc.member = m
         cc.save()
-        print('Saved survivor data')
+        ##print('Saved survivor data')
 
 
 
@@ -104,5 +105,5 @@ def member_entry(data):
                   is_member=data['spouse_is_member'])
     sp.member = m
     sp.save()
-    print('Saved spouse data')
+    ##print('Saved spouse data')
 
