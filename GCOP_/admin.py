aa8706a27c918844e5fa3964312003109eeae808
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-09 00:00:47 +0000

    deploy

--- GCOP_/admin.py
+++ GCOP_/admin.py
@@ -1,23 +1,23 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(AuthUser)
-admin.site.register(AuthGroup)
-admin.site.register(AuthGroupPermissions)
-admin.site.register(AuthPermission)
-admin.site.register(AuthUserGroups)
-admin.site.register(AuthUserUserPermissions)
-admin.site.register(Branches)
-admin.site.register(DjangoAdminLog)
-admin.site.register(DjangoContentType)
-admin.site.register(DjangoMigrations)
-admin.site.register(DjangoSession)
-admin.site.register(Groups)
-admin.site.register(Joinedgroups)
-admin.site.register(Member)
-admin.site.register(ChurchPositions)
-admin.site.register(Relations)
-
-
+# from django.contrib import admin
+# from .models import *
+#
+# # Register your models here.
+#
+# admin.site.register(AuthUser)
+# admin.site.register(AuthGroup)
+# admin.site.register(AuthGroupPermissions)
+# admin.site.register(AuthPermission)
+# admin.site.register(AuthUserGroups)
+# admin.site.register(AuthUserUserPermissions)
+# admin.site.register(Branches)
+# admin.site.register(DjangoAdminLog)
+# admin.site.register(DjangoContentType)
+# admin.site.register(DjangoMigrations)
+# admin.site.register(DjangoSession)
+# admin.site.register(Groups)
+# admin.site.register(Joinedgroups)
+# admin.site.register(Member)
+# admin.site.register(ChurchPositions)
+# admin.site.register(Relations)
+#
+#
