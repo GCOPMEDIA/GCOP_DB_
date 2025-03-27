e76ebb763d999803a8e3879518c7d6ddef6aba66
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-27 12:35:05 +0000

    images positioning

--- GCOP_/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import QrCodes
-
-class QRCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QrCodes
-        fields = ['id', 'code_data', 'scanned_at']
